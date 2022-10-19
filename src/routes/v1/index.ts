89d5a49f54d120b993dbf060f5c3e51963ac73a6
olashina201 <olashina201@gmail.com>
2022-10-19 16:45:48 +0100

    convert to typescript

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,5 @@
 const express = require('express');
-const authRoute = require('./auth.route');
-const userRoute = require('./user.route');
-const docsRoute = require('./docs.route');
-const config = require('../../config/config');
+
 
 const router = express.Router();
 
